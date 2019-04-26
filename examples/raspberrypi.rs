5bbc957022873758da22462089dd6054dcd57a63
Tim Small <tim@seoss.co.uk>
2019-04-26 09:43:25 +0100

    Reflect crate name.

--- examples/raspberrypi.rs
+++ examples/raspberrypi.rs
@@ -5,7 +5,7 @@ use hal::{Delay, I2cdev};
 use sht2x::SHT2x;
 
 fn main() {
-    println!("Hello, SHT21!");
+    println!("Hello, SHT2x!");
 
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let mut sht21 = SHT2x::new(dev, Delay);
